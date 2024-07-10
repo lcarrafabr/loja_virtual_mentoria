699220a585daff3fcdae9b390185bd3a07926bd2
LCarrafaBR <lcarrafa.br@gmail.com>
2024-07-10 14:15:18 -0300

    Criando modelo marca produto

---
+++ loja_virtual_mentoria/src/main/java/com/carrafasoft/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -0,0 +1,36 @@
+package com.carrafasoft.mentoria.lojavirtual.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categoria_produto")
+public class CategoriaProduto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeDesc;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CategoriaProduto that = (CategoriaProduto) o;
+
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
