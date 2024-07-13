fd07552c14606346fc601e25b72c49876ea9c52a
LCarrafaBR <lcarrafa.br@gmail.com>
2024-07-13 17:38:01 -0300

    Modelo de nota fiscal

---
+++ loja_virtual_mentoria/src/main/java/com/carrafasoft/mentoria/lojavirtual/model/NotaFiscalCompra.java
@@ -0,0 +1,48 @@
+package com.carrafasoft.mentoria.lojavirtual.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "nota_fiscal_compra")
+@SequenceGenerator(name = "seq_nota_fiscal_compra", sequenceName = "seq_nota_fiscal_compra", allocationSize = 1)
+public class NotaFiscalCompra implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
+    private Long id;
+
+    private String numeroNota;
+
+    private String serieNota;
+
+    private String descricaoObs;
+
+    private BigDecimal valorTotal;
+
+    private BigDecimal valorDesconto;
+
+    private BigDecimal valorIcms;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataCompra;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_pagar_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_fk"))
+    private  ContaPagar contaPagar;
+}
---
+++ loja_virtual_mentoria/src/main/java/com/carrafasoft/mentoria/lojavirtual/model/NotaItemProduto.java
@@ -0,0 +1,36 @@
+package com.carrafasoft.mentoria.lojavirtual.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "nota_item_produto")
+@SequenceGenerator(name = "seq_nota_item_produto", sequenceName = "seq_nota_item_produto", allocationSize = 1)
+public class NotaItemProduto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
+    private Long id;
+
+    @Column(nullable = false)
+    private Double quantidade;
+
+    @ManyToOne
+    @JoinColumn(name = "nota_fiscal_compra_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_fiscal_compra_fk"))
+    private NotaFiscalCompra notaFiscalCompra;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+    private Produto produto;
+
+
+}
